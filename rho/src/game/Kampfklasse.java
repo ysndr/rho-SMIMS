b2feae3f1bd5defa8ebc37ea0db9383dcd42283c
Yannik Sander <ysander98@gmail.com>
2016-09-05 21:32:11 +0200

    Tweaked Kampfklasse

--- rho/src/game/Kampfklasse.java
+++ rho/src/game/Kampfklasse.java
@@ -1,23 +1,43 @@
 package game;
+import java.util.Random;
 
 public class Kampfklasse {
-	private int aP;
-	private int vP;
 	
-	public Kampfklasse(int angreiferP, int verteidigerP) {
-		aP = angreiferP;
-		vP = verteidigerP;
+	private int angreifer;
+	private int verteidiger;
+	
+	
+	public Kampfklasse(int angreifer, int verteidiger) {
+
+		this.angreifer = angreifer;
+		this.verteidiger = verteidiger;
 	}
 	
-	public String ermittleSieger() {
-		if( (aP-vP) > 0) {
-			return "v";
-		} else {
-			return "a";
+	public String sieger() {
+		String sieger = null;
+		while(this.angreifer * this.verteidiger != 0 ){
+			
+			Random wuerfel = new Random();
+			int wurf1 = wuerfel.nextInt(6);
+			int wurf2 = wuerfel.nextInt(6);
+			
+			if(wurf1 > wurf2){
+				this.verteidiger--;
+			}
+			
+			else{
+				this.angreifer--;
+			}
+				
 		}
 	
+	if(this.angreifer==0){
+		sieger  = "v";
 	}
-	
-	
-	
+	if(this.verteidiger==0){
+		sieger = "a";
+	}
+	return sieger;
+	}	
+
 }
